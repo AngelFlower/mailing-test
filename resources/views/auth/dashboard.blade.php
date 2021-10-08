c30bfc992e0373c11c5fff7c9a5258fc778f9ada
2021 DSM31 Angel Flores Carlos <angelfc.contact@gmail.com>
2021-10-07 19:47:26 -0500

    Dashboard y favicon

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -1,39 +1,34 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Custom Auth in Laravel</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+@extends('layouts.auth')
 
-<body>
+@section('title', 'Usuario')
 
-    <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
-        <div class="container">
-            <a class="navbar-brand mr-auto" href="#">PositronX</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    @guest
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register-user') }}">Register</a>
-                    </li>
-                    @else
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('signout') }}">Logout</a>
-                    </li>
-                    @endguest
-                </ul>
+@section('css')
+
+    <link rel="stylesheet" href="{{ asset('/css/dashboard.css') }}">
+
+@endsection
+
+@section('content')
+    <div class="row h-100">
+        <div class="col-lg-6 p-5 v-center">
+            <div>
+                <div class="card shadow" style="width: 18rem;">
+                    <img src="{{ asset('svg/user.svg') }}" class="card-img-top p-5" alt="Imagen de usuario">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ Auth::user()->name }} </h5>
+                        <p class="card-text">{{ Auth::user()->email }} </p>
+                        <a href="{{ route('signout') }}" class="card-link">Cerar sesión</a>
+                        <a href="{{ url('/') }}" class="card-link">Ir a inicio</a>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-    @yield('content')
+        <div class="col-lg-6 p-5 v-center">
+            <div>
+                <h2 class="display-1">Ya estás registrado</h2>
+                <h3 class="display-5">Buena suerte!</h2>
+            </div>
+        </div>
+    </div>
 
-</body>
-
-</html>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <title>@yield('title')</title>
         @yield('css')
     </head>
